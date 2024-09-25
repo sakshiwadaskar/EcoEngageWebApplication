d7e6478de34aad508266e5f708cbde461d8d6336
sakshiWadaskar <wadaskar.s@northeastern.edu>
2024-09-25 16:18:34 -0400

    fixed mongod connection error.

--- app/ecoEngage-app/src/services/initiatives-service.ts
+++ app/ecoEngage-app/src/services/initiatives-service.ts
@@ -1,9 +1,10 @@
 import { BASE_URL } from "./auth-service"
+const API_URL = import.meta.env.VITE_BACKEND_SERVER_URL;
 
 export const InitiativeService = {
   async getInitiatives() {
     try {
-      const response = await fetch(`${BASE_URL}/initiatives`, {
+      const response = await fetch(`${API_URL}/initiatives`, {
         method: 'GET',
       });
 
