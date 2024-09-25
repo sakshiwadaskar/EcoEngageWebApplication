38ef4fb8ee5e7e9b0c6449342f14de5a12166d18
sakshiWadaskar <wadaskar.s@northeastern.edu>
2024-09-25 17:43:42 -0400

    fixed cors error.

--- service/app/app.js
+++ service/app/app.js
@@ -10,7 +10,7 @@ const connectToDatabase = async (mongoURI) => {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 50000, // 50 seconds timeout for server selection
+      serverSelectionTimeoutMS: 100000, // 50 seconds timeout for server selection
     });
     console.log('Successfully connected to MongoDB using Mongoose!');
   } catch (err) {
@@ -21,12 +21,22 @@ const connectToDatabase = async (mongoURI) => {
 
 const init = (app) => {
   // Middleware setup
-  const allowedOrigins = process.env.FRONTEND_SERVER_URL;
+  const allowedOrigins = [
+    'https://eco-engage-web-application.vercel.app', // Add your frontend URL without trailing /
+    // Add other origins if needed
+  ];
 
   app.use(cors({
-    origin: allowedOrigins || '*',
-    credentials: true, // Allow credentials (like cookies) to be sent
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // Allow credentials (cookies, headers)
   }));
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use('/uploads', express.static('uploads'));
